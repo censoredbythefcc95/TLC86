83540d8a6da76143b59b030a49561218361835f9
Steven <sastudillo6@gmail.com>
2022-12-08 01:58:46 -0700

    Suppressing strictQuery warning by setting global value to true.

--- config/db.js
+++ config/db.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+// ATTENTION: Mongoose 7 will bring strictQuery back to "false" when the update is released. 
+// You have the option of either suppressing the warning by using the following line code.
+// You also have the option of preparing for Mongoose 7 launch by setting the default value to "false"
+// In this instance, I have decided to suppress the warning, but I will include the code to set the value to "false" right below:
+// mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', true);
+
+
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
