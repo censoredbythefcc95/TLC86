b08e4041ec18502fa94d82d880ad55a986e9ffff
Steven <sastudillo6@gmail.com>
2022-12-08 18:03:38 -0700

    Created models folder and generated mongoose model for uers.

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const { popular } = require('ig/media');
-const { post } = require('request');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -16,11 +14,8 @@ app.get('/', (req, res) => res.send('API is running at this time 4:26pm December
 // Following routes correlate to the 4 files with users, profile, authentication and post
 
 app.use('/api/users', require('./routes/api/users'));
-
 app.use('/api/profile', require('./routes/api/profile'));
-
 app.use('/api/auth', require('./routes/api/auth'));
-
 app.use('/api/post', require('./routes/api/post'));
 
 app.listen(PORT, () => console.log(`Server initialized successfully! You are currently using port ${PORT}.`));
